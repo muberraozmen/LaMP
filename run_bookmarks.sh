444d040c775bb2fa309c9d8fdb25b20e76d24c96
muberraozmen <babam1harika>
2021-04-08 15:28:36 -0400

    cedar init

--- run_bookmarks.sh
+++ run_bookmarks.sh
@@ -3,14 +3,14 @@
 #SBATCH --gres=gpu:1              # Number of GPUs (per node)
 #SBATCH --mem=8G                  # memory (per node)
 #SBATCH --time=0-8:00            # time (DD-HH:MM)
-#SBATCH --output=output_bookmarks_prior.log       # output file
-#SBATCH --error=err_bookmarks_prior.log           # error file
+#SBATCH --output=output_bookmarks_none.log       # output file
+#SBATCH --error=err_bookmarks_none.log           # error file
 
-results_dir='/home/muberra/scratch/results_Apr7_bookmarks_prior/'
+results_dir='/home/muberra/scratch/results_Apr7_bookmarks_none/'
 dataroot='/home/muberra/scratch/data/'
 dataset=bookmarks
 
 module load python/3.6 cuda cudnn
 source ~/myenv/bin/activate
 
-python -u main.py -dataset $dataset -label_mask 'prior' -results_dir $results_dir -dataroot $dataroot
+python -u main.py -dataset $dataset -label_mask 'none' -results_dir $results_dir -dataroot $dataroot
