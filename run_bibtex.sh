862d7a44f3279ef45135403288050abb636f15fc
muberraozmen <babam1harika>
2021-05-05 17:25:19 -0400

    Revert "adding mrmp modules for exp"
    This reverts commit 57708d0a

--- run_bibtex.sh
+++ run_bibtex.sh
@@ -3,10 +3,10 @@
 #SBATCH --gres=gpu:1              # Number of GPUs (per node)
 #SBATCH --mem=8G                  # memory (per node)
 #SBATCH --time=0-0:30            # time (DD-HH:MM)
-#SBATCH --output=out.bibtex.lamp_mrmp.log       # output file
-#SBATCH --error=err.bibtex.lamp_mrmp.log           # error file
+#SBATCH --output=out.bibtex.lamp3.log       # output file
+#SBATCH --error=err.bibtex.lamp3.log           # error file
 
-results_dir='/home/muberra/scratch/results_May5_bibtex_lamp/'
+results_dir='/home/muberra/scratch/results_May1_bibtex_lamp3/'
 dataroot='/home/muberra/scratch/data/'
 dataset=bibtex
 
